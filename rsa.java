d0c1618a41cd1ab94a775fe06fff98470e17e1d4
Schahin Rajab <schahin_94@hotmail.com>
2018-10-11 14:41:35 +0200

    Update rsa.java

--- rsa.java
+++ rsa.java
@@ -12,52 +12,51 @@
 import java.util.*;
 
 public class rsa {
-	
+
 	public static void main(String[] args) {
-		  Scanner sc = new Scanner(System.in);
-	       long d, k, fi, n, e;
-	       int count;
-	       int t = sc.nextInt();
-    	   long[] pq = new long[2];
-	       
-	       for(int test = 0; test < t; test++) {
-	    	   count = 0;
-	    	   k = 0;
-	    	   fi = 0;
-	    	   n = sc.nextLong();
-	    	   e = sc.nextLong();
-	    	   pq[0] = pq[1] = 0;
-	    	   
-	    	   //calculate prime factors from n
-	    	   for(int j = 2; j < n; j++) {
-	    		   if(count >= 2)
-	    			   break;
-	    		  
-	    		   if(n % j == 0) {
-	    			   pq[count] = j;
-	    			   count++;
-	    		   }
-	    	   }
-	      
-	    	   //calculate fi
-	    	   fi = (pq[0] - 1) * (pq[1] - 1);
-	      
-	    	   //calculate k
-	    	   for(int i = 0; i < e; i++) {
-	    		   if((i * fi + 1) % e == 0) {
-	    			   k = i;
-	    			   break;
-	    		   }   
-	    	   }
-	       
-	    	   //calculate private key d
-	    	   d = (k * fi + 1) / e;
-	    	   
-	    	   System.out.println(d);
-	     
-	     //close for loop  
-	     }
-	        
+		Scanner sc = new Scanner(System.in);
+		long d, k, fi, n, e;
+		int count;
+		int t = sc.nextInt();
+		long[] pq = new long[2];
+
+		for(int test = 0; test < t; test++) {
+			count = 0;
+			k = 0;
+			fi = 0;
+			n = sc.nextLong();
+			e = sc.nextLong();
+			pq[0] = pq[1] = 0;
+
+			//calculate prime factors from n
+			for(int j = 2; j < n; j++) {
+				if(count >= 2)
+					break;
+
+				if(n % j == 0) {
+					pq[count] = j;
+					count++;
+				}
+			}
+
+			//calculate fi
+			fi = (pq[0] - 1) * (pq[1] - 1);
+
+			//calculate k
+			for(int i = 0; i < e; i++) {
+				if((i * fi + 1) % e == 0) {
+					k = i;
+					break;
+				}   
+			}
+
+			//calculate private key d
+			d = (k * fi + 1) / e;
+
+			System.out.println(d);
+
+		//close for loop  
+		}
 	//close main        		
 	}
 //close class
